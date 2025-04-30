e1d6a9305b32befc59302cb6b2a2c162a6068743
amara343 <amara.alphasquad@gmail.com>
2025-04-30 13:15:33 +0500

    file claim update

--- lib/screens/files_claim_screen.dart
+++ lib/screens/files_claim_screen.dart
@@ -105,7 +105,7 @@ class FilesClaimScreen extends StatelessWidget {
                                       ),
                                     ),
                                   ],
-                                ),
+                                ), 
                               ),
                           ],
                         ),
